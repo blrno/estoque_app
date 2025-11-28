e1813aa7b213971a8d7bd9c1a72224f39dc4fb84
claudio-py <claudio.3777@gmail.com>
2025-11-28 15:25:20 -0300

    custom auth

--- users/urls.py
+++ users/urls.py
@@ -4,4 +4,6 @@ from .views import *
 app_name = "users"
 urlpatterns = [
     path("login/", Login.as_view(), name="login"),
+    path("logout/", Logout.as_view(), name="logout"),
+    path("register/", Register.as_view(), name="register"),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,28 @@
 from django.shortcuts import render
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.views.generic.edit import FormView
+from .forms import RegisterUserForm
 
 
 # Create your views here.
 class Login(LoginView):
     template_name = "users/login/index.html"
+
+
+class Logout(LogoutView):
+    next_page = "/"
+
+
+class Register(FormView):
+    template_name = "users/register/index.html"
+    form_class = RegisterUserForm
+    success_url = "/users/login/"
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            return self.form_invalid(form)
